345da6bcc1f8a76160aebc89681c8e4c06547109
Vinicius Victor <vine.victordev@gmail.com>
2022-10-25 22:32:50 -0300

    Função Escolha De Opções

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Cadastro e Login de Usuario
 var indexName = 0, indexPassword = 0;
-
+var close = true;
 var arrayName = [];
 var arrayPassword = [];
 
@@ -14,3 +14,16 @@ function NameAndPassword() {
     indexName++;
     indexPassword++;
 };
+
+function ChooseOptions() {
+    var choice = parseInt(prompt("1 - Cadastro, 2 - Fazer Login, 3 - Excluir Cadastro, 4 -Encerrar Programa"));
+    if (choice == 1) {
+        NameAndPassword();
+    } else if (choice == 2) {
+        Login();
+    } else if (choice == 3) {
+        DeleteRegistration();
+    } else if (choice == 4) {
+        close = false;
+    }
+}
